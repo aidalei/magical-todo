1a0f37d0ab6b2fdcc7a136cabfab774d0a131b77
aidalei <j.aida@leihauoli.com>
2020-06-24 23:25:29 +0900

    add emotion and uid

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import Top from './components/pages/Top';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Top />
-      <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: '30px',
   },
 });
--- components/pages/Top.tsx
+++ components/pages/Top.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { View, Text } from "react-native";
+import v4 from 'uid'
+
+// TODO React-nativeで書き直す
 
 const Top = () => {
   return (
-    <View>
-      <Text>New Top</Text>
-    </View>
+    <div>
+        <input></input>
+        <button>add</button>
+        <p>{v4()}</p>
+        <ul>
+            <li><p>some TODO</p><button>delete</button></li>
+            <li><p>some TODO</p><button>delete</button></li>
+            <li><p>some TODO</p><button>delete</button></li>
+            <li><p>some TODO</p><button>delete</button></li>
+            <li><p>some TODO</p><button>delete</button></li>
+            <li><p>some TODO</p><button>delete</button></li>
+        </ul>
+    </div>
   );
 };
 
