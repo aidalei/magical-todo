5f8e8bd2754eb3c2d9d0fbc589d3e683ae446cf7
aidalei <j.aida@leihauoli.com>
2020-06-24 23:06:46 +0900

    done scaffolding

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Top from './components/pages/Top';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Top />
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
---
+++ components/pages/Top.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Top = () => {
+  return (
+    <View>
+      <Text>New Top</Text>
+    </View>
+  );
+};
+
+export default Top;
